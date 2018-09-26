832d52826c0845b2d8e20efb68aebd0992dec45e
rainzha <rainzha@163.com>
2018-09-27 01:36:02 +0800

    finished use run.sh.

--- spima-simpleservice/run.sh
+++ spima-simpleservice/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting spima-simpleservice "
 echo "********************************************************"
-java -jar /usr/local/simpleservice/$1
+java -jar /usr/local/simpleservice/$1 -Djava.security.egd=file:/dev/./urandom
