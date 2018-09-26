0dbed7e56e02c1c0d048896490d1962d44fe18bc
rainzha <rainzha@163.com>
2018-09-27 01:25:19 +0800

    finished use run.sh.

--- spima-simpleservice/run.sh
+++ spima-simpleservice/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting spima-simpleservice "
 echo "********************************************************"
-java -jar /usr/local/simpleservice/spima-simpleservice-1.0.jar
+java -jar /usr/local/simpleservice/$1
